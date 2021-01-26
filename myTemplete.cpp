91550b70b187fe9984cade0a570a9fe9aff1cbab
SynTaxOp <69120068+SynTaxOp@users.noreply.github.com>
2021-01-27 01:11:50 +0530

    Create myTemplete.cpp

---
+++ myTemplete.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<pair<int, int>> vp;
+typedef pair<long long, long long> pl;
+typedef map<int, int> mi;
+typedef unordered_map<int,int>umi;
+typedef unordered_map<int,vector<int>>graph;
+typedef unordered_map<int,vector<pi>>wgraph;
+#define fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
+#define fa(i, N) for (auto i : N)
+#define all(X) X.begin(), X.end()
+#define INF 2e9
+#define vin(name,N) vi name(N);fo(i,0,N){cin>>name[i];}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int tc = 1;
+  cin >> tc;
+  while (tc--)
+  {  
+    
+  }  
+}
