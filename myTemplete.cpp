8e9caba4a087444a04728fc5c78433eb6c05c355
SynTaxOp <69120068+SynTaxOp@users.noreply.github.com>
2021-09-06 10:58:13 +0530

    Update myTemplete.cpp

--- myTemplete.cpp
+++ myTemplete.cpp
@@ -1,34 +1,42 @@
+//Code written by:srejanbera
+
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define int long long
 typedef long double ld;
 typedef pair<int, int> pi;
 typedef vector<int> vi;
-typedef vector<long long> vl;
-typedef vector<pair<int, int>> vp;
-typedef pair<long long, long long> pl;
+typedef vector<pi> vp;
 typedef map<int, int> mi;
 typedef unordered_map<int,int>umi;
+typedef map<char,int>mc;
 typedef unordered_map<int,vector<int>>graph;
 typedef unordered_map<int,vector<pi>>wgraph;
+typedef priority_queue<int> pq;
 #define fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
 #define fa(i, N) for (auto i : N)
 #define all(X) X.begin(), X.end()
-#define INF 2e9
+#define ceill(a,b) ((a)/(b) + (((a)%(b))!=0))
+#define bin(n) bitset<32>(n).to_string()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define el "\n"
 #define vin(name,N) vi name(N);fo(i,0,N){cin>>name[i];}
+#define print(V) fa(i,V){cout<<i<<" ";}cout<<el;
+#define deb(x) cout<<#x<<" "<<x<<el;
 
-int main()
+void solve()
 {
-#ifndef ONLINE_JUDGE
-freopen("input.txt","r",stdin);
-freopen("output.txt","w",stdout);
-#endif
+   // Code goes here.
+}
+int32_t main()
+{
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   int tc = 1;
   cin >> tc;
   while (tc--)
   {  
-    
-  }  
+    solve();
+  }
 }
